da624300fbcdbf7f9d1bd888c539f562415fa205
Htike <htikehlaaung1234@gmail.com>
2026-05-07 10:37:15 +0100

    add cors_allowed_origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,14 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
